b902391c4a369f9da0d6b692df0e56e22a7951a2
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-08-25 17:07:18 +0530

    ref : theme

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg shadow sticky-top bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand fw-400" href="/">Shubhchintak Technology </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
