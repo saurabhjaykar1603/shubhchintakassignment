fbcffb34c588413ed8306c85ad5710c272b3d917
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-08-25 15:48:22 +0530

    feat: added search functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
-
 function App() {
   const [data, setData] = useState([]);
-
-
+  const [searchText, setSearchText] = useState('');
 
   const loadTask = async () => {
     const { data } = await axios.get("https://64e8643899cf45b15fdf7bdd.mockapi.io/users")
@@ -15,36 +13,40 @@ function App() {
 
   useEffect(() => {
     loadTask();
-
   }, [])
 
+  const filteredData = data.filter(user => user.name.toLowerCase().includes(searchText.toLowerCase()));
 
   return (
     <div className="container">
-      {
-        (data).map((user, index) => {
-          const { id, name, avatar, createdAt } = user;
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+        />
+      </div>
+      {filteredData.map((user, index) => {
+        const { id, name, avatar, createdAt } = user;
 
-          return (
-            <div className='d-flex justify-content-center '>
-              <div className="card custom-card w-75  mt-5 shadow" key={id} >
-                <div className="row">
-                  <div className="col col-md-6">
-                    <img src={avatar} class="custom-img-class h-50 mt-3 ms-5" alt="..." />
-
-                  </div>
-                  <div className="col col-md-6">
-                    <h5 className="car fw-bold">{name}</h5>
-                    <p className="card-text fw-bold ">{id}</p>
-                    <p className="card-text ">{createdAt}</p>
-                  </div>
+        return (
+          <div className='d-flex justify-content-center ' key={id}>
+            <div className="card custom-card w-75  mt-5 shadow">
+              <div className="row">
+                <div className="col col-md-6">
+                  <img src={avatar} className="custom-img-class h-50 mt-3 ms-5" alt="..." />
                 </div>
-
+                <div className="col col-md-6">
+                  <h5 className="car fw-bold">{name}</h5>
+                  <p className="card-text fw-bold ">{id}</p>
+                  <p className="card-text ">{createdAt}</p>
+                </div>
               </div>
             </div>
-          )
-        })
-      }
+          </div>
+        )
+      })}
     </div>
   );
 }
